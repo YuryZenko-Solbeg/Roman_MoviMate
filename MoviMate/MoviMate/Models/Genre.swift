80408fdf939a7b529c39753691ef0c1e5aaa8f63
roman25085 <roman.alikevich@solbeg.com>
2021-08-10 10:33:29 +0300

    add: genre model

---
+++ MoviMate/MoviMate/Models/Genre.swift
@@ -0,0 +1,16 @@
+//
+//  Genre.swift
+//  MoviMate
+//
+//  Created by Roman Alikevich on 10.08.21.
+//
+
+import UIKit
+
+struct Genre {
+    var id: Int = 0
+    var name: String = ""
+    var url: GenreImageUrl
+}
+
+
