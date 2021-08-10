302c878929f8dddfd5379858e35a564046e17d01
roman25085 <roman.alikevich@solbeg.com>
2021-08-10 10:32:35 +0300

    add: created genre image enum of urls

---
+++ MoviMate/MoviMate/Enums/GenreImageUrl.swift
@@ -0,0 +1,27 @@
+//
+//  GenreImageConstant.swift
+//  MoviMate
+//
+//  Created by Roman Alikevich on 10.08.21.
+//
+
+import UIKit
+
+enum GenreImageUrl: String {
+    case action = "action"
+    case adventure = "adventure"
+    case cartoon = "cartoon"
+    case crime = "crime"
+    case comedy = "comedy"
+    case documentary = "documentary"
+    case drama = "drama"
+    case family = "family"
+    case historical = "historical"
+    case horror = "horror"
+    case detective = "detective"
+    case tv_movie = "tv_movie"
+    case thriller = "thriller"
+    case military = "military"
+    case western = "western"
+    
+}
