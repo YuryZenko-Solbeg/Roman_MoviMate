0e188a5ab2e9e71b8531b4240abb683124d427f3
roman25085 <roman.alikevich@solbeg.com>
2021-08-12 12:55:29 +0300

    add: added enum of network urls

---
+++ MoviMate/MoviMate/Enums/NetworkUrl.swift
@@ -0,0 +1,14 @@
+//
+//  NetworkUrl.swift
+//  MoviMate
+//
+//  Created by Roman Alikevich on 10.08.21.
+//
+
+import UIKit
+
+enum NetworkUrl: String {
+    case getListOfGenre = "https://api.themoviedb.org/3/genre/movie/list"
+    case getListOfMovieByGenreId = "https://api.themoviedb.org/3/list/"
+    case getMovieDescriptionByMovieId = "https://api.themoviedb.org/3/movie/"
+}
