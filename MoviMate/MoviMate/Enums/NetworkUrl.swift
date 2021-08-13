bb37055f2cb904caaab11fb441495fc7a38d8a71
Roman <roma.aikevich@mail.ru>
2021-08-13 12:51:52 +0300

    add: added getMoviePostImage case for networking

--- MoviMate/MoviMate/Enums/NetworkUrl.swift
+++ MoviMate/MoviMate/Enums/NetworkUrl.swift
@@ -9,6 +9,7 @@ import UIKit
 
 enum NetworkUrl: String {
     case getListOfGenre = "https://api.themoviedb.org/3/genre/movie/list"
-    case getListOfMovieByGenreId = "https://api.themoviedb.org/3/list/"
+    case getListOfMovieByGenreId = "https://api.themoviedb.org/3/list"
     case getMovieDescriptionByMovieId = "https://api.themoviedb.org/3/movie/"
+    case getMoviePostImage = "https://image.tmdb.org/t/p/"
 }
