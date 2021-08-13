ddf45fa57abe53cad8b38e13b9342c1fd527ff6d
Roman <roma.aikevich@mail.ru>
2021-08-13 16:32:32 +0300

    add: added basic view controller

--- MoviMate/MoviMate/Controller/ViewController.swift
+++ MoviMate/MoviMate/Controller/ViewController.swift
@@ -9,11 +9,71 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet private weak var startButton: UIButton!
+    @IBOutlet private weak var settingsButton: UIButton!
+    
+    var networkManager: NetworkManager {
+        return NetworkManagerImpl(fetcher: NetworkFetcherImpl(service: NetworkServiceImpl()))
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        startButton.layer.cornerRadius = 8
+        settingsButton.layer.cornerRadius = 8
+        
+        navigationController?.setNavigationBarHidden(true, animated: false)
+//        let mutabletitle = NSMutableAttributedString(string: "START")
+
+        let attributedTitle = NSAttributedString(string: "START GAME", attributes:
+                                                    [NSAttributedString.Key.kern: 10.0,
+                                                     NSAttributedString.Key.font: UIFont.systemFont(ofSize: 10),
+                                                     NSAttributedString.Key.foregroundColor: UIColor.white
+        ])
+        startButton.setAttributedTitle(attributedTitle, for: .normal)
     }
+    
+    @IBAction func start(_ sender: Any) {
+        let genreList = networkManager.getListOfGenre(completionHandler: { result in
+            
+            DispatchQueue.main.async {
+                switch result {
+                case .success(let genreList):
+                    self.openGenreViewController(genreList)
+                case .failure(let error):
+                    print(error)
+                }
+            }
+        })
+    }
+    @IBAction func openSetting(_ sender: Any) {
+        pushSettingViewController()
+    }
+}
 
+private extension ViewController {
+    
+    func openGenreViewController(_ list: GenreList) {
+        
+        let storyBoard = UIStoryboard(name: "Main", bundle: nil)
+        
+        guard let genreView = storyBoard.instantiateViewController(identifier: "showListOfGenres") as? GenreViewController else {
+            return
+        }
+        
+        genreView.genreList = list.genres
+        
+        navigationController?.pushViewController(genreView, animated: true)
+    }
+    
+    func pushSettingViewController() {
+        
+        let storyBoard = UIStoryboard(name: "Main", bundle: nil)
 
+        guard let settingView = storyBoard.instantiateViewController(identifier: "languageSetings") as? SettingsViewController else {
+            return
+        }
+        
+        navigationController?.pushViewController(settingView, animated: true)
+    }
 }
 
