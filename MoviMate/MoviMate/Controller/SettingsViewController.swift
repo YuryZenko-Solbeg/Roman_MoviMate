54559c8ddef31ee945c9371d30da2affb4f10c8b
roman25085 <roman.alikevich@solbeg.com>
2021-08-16 14:31:33 +0300

    add: added collection view for settings present

--- MoviMate/MoviMate/Controller/SettingsViewController.swift
+++ MoviMate/MoviMate/Controller/SettingsViewController.swift
@@ -8,11 +8,56 @@
 import UIKit
 
 class SettingsViewController: UIViewController {
+    
+    var settingArray = ["LANGUAGE OPTION", "RESOLUSION OPTIONS"]
 
+    @IBOutlet weak var collectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        
+//        collectionView.register(UINib(nibName: "flagView", bundle: nil), forCellWithReuseIdentifier: "flagsView")
+//        collectionView.register(UINib(nibName: "segmentView", bundle: nil), forCellWithReuseIdentifier: "segmentsView")
     }
     
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        
+        
+    }
+}
+
+extension SettingsViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        
+        return settingArray.count
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "languageOption", for: indexPath) as? LanguageOptionCollectionViewCell else {
+            return UICollectionViewCell()
+        }
+        
+        cell.layer.cornerRadius = 8
+        
+        if indexPath.item == 0 {
+            cell.addCellSubview(cellState: .lan)
+        } else if indexPath.item == 1 {
+            cell.addCellSubview(cellState: .segment)
+        } else {
+            fatalError("Didn't determine changing")
+        }
+        
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        return CGSize(width: view.frame.width * 0.87, height: view.frame.width * 0.54)
+    }
 }
