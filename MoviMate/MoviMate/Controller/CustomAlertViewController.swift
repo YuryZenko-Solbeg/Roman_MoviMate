98b59011470d8c875713afff9c8d2928a86482a4
roman25085 <roman.alikevich@solbeg.com>
2021-08-16 14:29:27 +0300

    feat: added custom alert controller

---
+++ MoviMate/MoviMate/Controller/CustomAlertViewController.swift
@@ -0,0 +1,19 @@
+//
+//  CustomAlertViewController.swift
+//  MoviMate
+//
+//  Created by Roman Alikevich on 16.08.2021.
+//
+
+import UIKit
+
+class CustomAlertViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func dismiss(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+}
