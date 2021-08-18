14f27d154867e1de7374ac03ce64cdeffbbc4dff
roman25085 <roman.alikevich@solbeg.com>
2021-08-18 15:25:58 +0300

    refactor: replace segment control action logic to  SegmentControlView

--- MoviMate/MoviMate/SegmentControlView.swift
+++ MoviMate/MoviMate/SegmentControlView.swift
@@ -11,6 +11,44 @@ class SegmentControlView: UIView {
     
     @IBOutlet weak var segmentView: UISegmentedControl!
     
+    enum PixelSize: Int {
+        
+        case small = 0
+        case meddium
+        case high
+        
+        func getSize() -> Int {
+            
+            switch self {
+
+            case .small:
+
+                return 200
+            case .meddium:
+
+                return 300
+            case .high:
+
+             return 500
+            }
+        }
+        
+        func getElement(size: Int) -> PixelSize {
+            
+            switch size {
+            
+            case 200:
+                return .small
+            case 300:
+                return .meddium
+            case 500:
+                return .high
+            default:
+                fatalError("Incorrect size in PixelSize enum")
+            }
+        }
+    }
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         
@@ -27,6 +65,23 @@ class SegmentControlView: UIView {
         segmentView.insertSegment(withTitle: "MEDDIUM", at: 1, animated: false)
         segmentView.insertSegment(withTitle: "HIGH", at: 2, animated: false)
         
-        segmentView.selectedSegmentIndex = 0
+
+        let size = UserDefaultManager.shared.getPixelSize()
+        let pixelSize = PixelSize.getElement(PixelSize(rawValue: 0)!)
+        segmentView.selectedSegmentIndex = pixelSize(size!).rawValue
+        
+        segmentView.addTarget(self, action: #selector(self.swipeHandler(_:)), for: .valueChanged)
+    }
+    
+    @objc func swipeHandler(_ segment: UISegmentedControl) {
+
+        let index = segment.selectedSegmentIndex
+        let pixelElement = PixelSize(rawValue: index)!
+        let size = PixelSize.getSize(pixelElement)
+        self.reloadSegment(size: size())
+    }
+    
+    func reloadSegment(size: Int) {
+        UserDefaultManager.shared.updateStorage([UserDefaultManager.WareHouseType.pixelSize.rawValue:size])
     }
 }
