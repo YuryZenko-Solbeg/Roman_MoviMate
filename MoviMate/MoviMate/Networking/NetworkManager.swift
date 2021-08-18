a75de9239bc5e59219ae556688990ed013fa5fe0
roman25085 <roman.alikevich@solbeg.com>
2021-08-18 15:24:58 +0300

    fix: fixed replace string constants  on static properties

--- MoviMate/MoviMate/Networking/NetworkManager.swift
+++ MoviMate/MoviMate/Networking/NetworkManager.swift
@@ -41,6 +41,10 @@ class NetworkManagerImpl: NetworkManager {
     var urlComponents: URLComponents
     var fetcher: NetworkFetcher!
     
+    private static let apiKeyParam = "api_key"
+    private static let languageParam = "language"
+    private static let apiKey = "9dcb2fc7f9c7799a5d892a673fa4d40c"
+    
     init(fetcher: NetworkFetcher) {
         urlComponents = URLComponents(string: "")!
         self.fetcher = fetcher
@@ -48,28 +52,31 @@ class NetworkManagerImpl: NetworkManager {
     
     func getListOfGenre(completionHandler: @escaping (Result<GenreList, Error>) -> Void) {
         urlComponents = URLComponents(string: NetworkUrl.getListOfGenre.rawValue)!
-        urlComponents.queryItems = [URLQueryItem(name: "api_key", value: "9dcb2fc7f9c7799a5d892a673fa4d40c"),
-                                    URLQueryItem(name: "language", value: "en")]
+        
+        urlComponents.queryItems = [URLQueryItem(name: NetworkManagerImpl.apiKeyParam, value: NetworkManagerImpl.apiKey),
+                                    URLQueryItem(name: NetworkManagerImpl.languageParam, value: UserDefaultManager.shared.getLanguage())]
         fetcher.send(url: urlComponents.url!, completionHandler: completionHandler)
     }
     
     func getListOfMovie(id: Int, completionHandler: @escaping (Result<MovieList, Error>) -> Void) {
         urlComponents = URLComponents(string: "\(NetworkUrl.getListOfMovieByGenreId.rawValue)/\(String(id))")!
-        urlComponents.queryItems = [URLQueryItem(name: "api_key", value: "9dcb2fc7f9c7799a5d892a673fa4d40c"),
-                                    URLQueryItem(name: "language", value: "en")]
+        
+        urlComponents.queryItems = [URLQueryItem(name: NetworkManagerImpl.apiKeyParam, value: NetworkManagerImpl.apiKey),
+                                    URLQueryItem(name: NetworkManagerImpl.languageParam, value: UserDefaultManager.shared.getLanguage())]
         fetcher.send(url: urlComponents.url!, completionHandler: completionHandler)
     }
     
     func getMovieById(id: Int, completionHandler: @escaping (Result<MovieDescription, Error>) -> Void) {
         urlComponents = URLComponents(string: "\(NetworkUrl.getMovieDescriptionByMovieId.rawValue)\(String(id))")!
-        urlComponents.queryItems = [URLQueryItem(name: "api_key", value: "9dcb2fc7f9c7799a5d892a673fa4d40c"),
-                                    URLQueryItem(name: "language", value: "en")]
+        
+        urlComponents.queryItems = [URLQueryItem(name: NetworkManagerImpl.apiKeyParam, value: NetworkManagerImpl.apiKey),
+                                    URLQueryItem(name: NetworkManagerImpl.languageParam, value: UserDefaultManager.shared.getLanguage())]
         print(urlComponents.url!)
         fetcher.send(url: urlComponents.url!, completionHandler: completionHandler)
     }
     
     func getMoviePoster(path: String) -> Data {
-        urlComponents = URLComponents(string: "\(NetworkUrl.getMoviePostImage.rawValue)w500/\(String(path))")!
+        urlComponents = URLComponents(string: "\(NetworkUrl.getMoviePostImage.rawValue)w\( UserDefaultManager.shared.getPixelSize()!)/\(String(path))")!
         
         guard let data = try? Data(contentsOf: urlComponents.url!) else {
             return Data()
